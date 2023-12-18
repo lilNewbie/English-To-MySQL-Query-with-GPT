fb486dab7f439ccb5cb1d46f2424929be8b47045
Alan Jacob Anil <90834922+lilNewbie@users.noreply.github.com>
2023-12-18 16:38:46 +0530

    Update secrets1.py
    Adding Azure Endpoint and password to access MySQL database

--- secrets1.py
+++ secrets1.py
@@ -2,4 +2,6 @@ def get_keys():
     openai_api_key = "YOUR_KEY"
     azure_api_key="YOUR_KEY"
     azure_api_version='YOUR_VERSION'
-    return openai_api_key, azure_api_key, azure_api_version
+    db_pwd = "MYSQL_PWD"
+    azure_ep = "YOUR AZURE ENDPOINT"
+    return openai_api_key, azure_api_key, azure_api_version, azure_api_version, db_pwd
