8358f16087abb2eaa36f98ded3075004992d2300
Alan Jacob Anil <90834922+lilNewbie@users.noreply.github.com>
2023-12-18 15:26:55 +0530

    Update server.py
    SQL query has been generated successfully

--- server.py
+++ server.py
@@ -5,11 +5,13 @@ from flask import Flask, render_template, request, jsonify
 import json
 import requests
 import mysql.connector
-from secrets import get_keys
+from secrets1 import get_keys
 
-openai_api_key, azure_api_key, azure_api_version = get_keys()
-model = 'gpt-3.5-turbo-0613'
+openai_api_key, azure_api_key, azure_api_version, db_pwd = get_keys()
+model = 'gpt-3.5-turbo-1106'
+
 client = OpenAI(api_key=openai_api_key)
+
 client2 = AzureOpenAI(
     azure_endpoint="https://dager1.openai.azure.com/",
     api_key=azure_api_key,
@@ -19,8 +21,8 @@ client2 = AzureOpenAI(
 #load the MySQL database
 mydb = mysql.connector.connect(
     host="localhost",
-    user="",
-    password=""
+    user="root",
+    password=db_pwd
 )
 
 app = Flask(__name__)
@@ -36,29 +38,29 @@ def index():
 @app.route("/get_response", methods=['GET', 'POST'])
 def get_response():
     if request.method == 'POST':
+        #message = request
         message = request.data.decode('utf-8')
-    else:
+    else:  # This handles the GET request case
         message = request.args.get("message")
 
     message=json.loads(message)
     tools = message['tools']
-    msg = [{"role":"user","content":message['message']}]
+    msg = [{ "role": "system","content": "Given the following SQL tables, your job is to write queries given a users request.\n  \n  create table mood_table(\n  sno int not null,\n  name varchar(20),\n  mood varchar(15),\n  primary key (sno)\n  );"}]
+    msg.append({"role":"user","content":message['message']})
 
     response="hehe"
     if 'sql' in message['message'].lower():
-        response = client2.chat.completions.create(
-        model="gpt-3.5-turbo-1106",
-        messages=msg,
-        tools=tools,
-        tool_choice="auto",
-    )
+        response = client.chat.completions.create(
+            model=model,
+            messages=msg,
+            temperature=0.7,
+            max_tokens=64,
+            top_p=1
+        )
+        
     
-    response_content = response.choices[0].message.tool_calls[0].function.arguments
-    r_str = response_content.split(":")
-    r_str = r_str[1][1:-2]
-    
-    print(r_str)
-    return r_str
+    response_content = response.choices[0].message.content
+    return response_content
 
 if __name__ == "__main__":
     app.run(debug=True)
