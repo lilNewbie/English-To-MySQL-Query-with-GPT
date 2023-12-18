7c7d58fbb1e1837e21c2d8e9fd9af4cdf0dbccf2
Alan Jacob Anil <90834922+lilNewbie@users.noreply.github.com>
2023-12-18 16:34:52 +0530

    Update server.py
    All functionality added

--- server.py
+++ server.py
@@ -7,13 +7,14 @@ import requests
 import mysql.connector
 from secrets1 import get_keys
 
-openai_api_key, azure_api_key, azure_api_version, db_pwd = get_keys()
+# load env vars from .env file
+openai_api_key, azure_api_key, azure_api_version, db_pwd, azure_ep = get_keys()
 model = 'gpt-3.5-turbo-1106'
 
 client = OpenAI(api_key=openai_api_key)
 
 client2 = AzureOpenAI(
-    azure_endpoint="https://dager1.openai.azure.com/",
+    azure_endpoint=azure_ep,
     api_key=azure_api_key,
     api_version=azure_api_version
     )
@@ -38,29 +39,55 @@ def index():
 @app.route("/get_response", methods=['GET', 'POST'])
 def get_response():
     if request.method == 'POST':
-        #message = request
         message = request.data.decode('utf-8')
     else:  # This handles the GET request case
         message = request.args.get("message")
 
     message=json.loads(message)
-    tools = message['tools']
-    msg = [{ "role": "system","content": "Given the following SQL tables, your job is to write queries given a users request.\n  \n  create table mood_table(\n  sno int not null,\n  name varchar(20),\n  mood varchar(15),\n  primary key (sno)\n  );"}]
-    msg.append({"role":"user","content":message['message']})
+    msg_query = [{ "role": "system","content": "Given the following SQL tables, your job is to write queries given a users request.\n  \n  create table mood_table(\n  sno int not null,\n  name varchar(20),\n  mood varchar(15),\n  primary key (sno)\n  );"}]
+    msg_query.append({"role":"user","content":message['message']})
 
     response="hehe"
-    if 'sql' in message['message'].lower():
-        response = client.chat.completions.create(
-            model=model,
-            messages=msg,
-            temperature=0.7,
-            max_tokens=64,
-            top_p=1
-        )
-        
+    response = client.chat.completions.create(
+        model=model,
+        messages=msg_query,
+        temperature=0.7, 
+        max_tokens=64,
+        top_p=1
+    )
     
     response_content = response.choices[0].message.content
-    return response_content
+    
+    mycursor = mydb.cursor()
+    mycursor.execute("use test_db;")
+    mycursor.execute(response_content.split("```sql")[1][:-3])
+    
+    x = [i for i in mycursor][0][0]
+
+    #UNCOMMENT THESE LINES TO VIEW THE SQL QUERY AND THE MOOD RETURNED
+    #print(response_content.split("```sql")[1][:-3])
+    #print(x)
+
+    msg_mood = [{
+        "role":"system",
+        "content":"Give an answer to the prompt in a sentence."
+    },
+    {
+        "role":"user",
+        "content":message['message'] + "\n  " + x
+    }]
+
+
+    response_mood = client.chat.completions.create(
+        model=model,
+        messages=msg_mood,
+        temperature=0.7,
+        max_tokens=64,
+        top_p=1
+    )
+
+    response_content_mood = response_mood.choices[0].message.content
+    return response_content_mood
 
 if __name__ == "__main__":
     app.run(debug=True)
